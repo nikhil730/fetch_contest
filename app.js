b1eab1b71019686aaac144beaf5bd97dfc387ea7
nikhil730 <nikhilbhalla196@gmail.com>
2023-05-24 01:20:06 +0530

    ..

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ function fetchdata() {
     });
 }
 fetchdata();
-setInterval(fetchdata, 60000 * 60 * 12);
+setInterval(fetchdata, 10000);
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
